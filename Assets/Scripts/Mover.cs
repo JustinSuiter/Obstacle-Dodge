1b42c21bdc3b6f9a1bdc4e385e9697ad062f4e21
Justin S <justinsuiter4@gmail.com>
2025-09-19 08:31:57 -0400

    Learning Variable Fields and Adding Player controller

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0f;
-    float yValue = 0.01f;
-    float zValue = 0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical");
+
         transform.Translate(xValue, yValue, zValue);
+        
+
     }
 }
