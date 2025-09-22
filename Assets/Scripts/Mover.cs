4bc3db58d88a112aa0399f0ce76eb4ba7c34d03d
Justin S <justinsuiter4@gmail.com>
2025-09-22 08:32:30 -0400

    Learned Cinemachine, DeltaTime, Rigidbody Collisions
    Learned how to use cinemachine for cameras and learned how rigid body collisions and delta time works.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 public class Mover : MonoBehaviour
+
 {
+    [SerializeField] float moveSpeed = 10f;
     void Start()
     {
         
@@ -9,9 +11,9 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical");
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, yValue, zValue);
         
