7ef112c4199529697fff659d4d0d1ca23d3e7b29
Justin Suiter <justinsuiter4@gmail.com>
2025-09-25 23:23:49 -0400

    Destroy GameObject
    Learned how to destroy a game object when it reaches its destination or fulfills its purpose/

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -10,12 +10,29 @@ public class FlyAtPlayer : MonoBehaviour
     void Start()
     {
         playerPos = player.transform.position;
+        
     }
 
 
     void Update()
     {
+        flyAtPlayer();
+        DestroyWhenReached();
+    }
+
+    void flyAtPlayer()
+    {
         float flySpeed = Time.deltaTime * speed;
         transform.position = Vector3.MoveTowards(transform.position, playerPos, flySpeed);
+        
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPos)
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
